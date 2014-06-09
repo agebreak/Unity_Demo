2b8459a3836915c983c5b859dcecc65d1e3a09b0
agebreak <agebreak@naver.com>
2014-06-09 19:14:41 +0900

    텍스쳐UV 애니메이션 추가
    텍스쳐UV 애니메이션 추가

---
+++ Assets/Floor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+	public Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 textureOffset = renderer.material.mainTextureOffset;
+		textureOffset += offset * Time.deltaTime;
+		renderer.material.mainTextureOffset = textureOffset;
+	
+	}
+}
